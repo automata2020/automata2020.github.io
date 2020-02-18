079eb5af56c8e458cbfabc2177cf113694dcdc5a
automata2020 <61178018+automata2020@users.noreply.github.com>
2020-02-18 12:02:16 +0530

    added base website

---
+++ script.js
@@ -0,0 +1,20 @@
+// const belowMainSection = document.getElementById("belowMain")
+// const navFixedAdder = document.getElementById("fixer")
+// console.log()
+
+
+// window.onscroll = function(){
+//     myfunc();
+// }
+
+
+
+// function myfunc(){
+//     if(window.pageYOffset>belowMainSection.offsetTop){
+//         navFixedAdder.classList.add("sticky")
+//     }else{
+//         navFixedAdder.classList.remove("sticky")
+//     }
+// }
+
+
